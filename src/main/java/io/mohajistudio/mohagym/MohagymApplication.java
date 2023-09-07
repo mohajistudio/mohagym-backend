cde6e7eaba0b2c78f5fc471c0e072e4517141920
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2023-09-07 10:00:55 +0900

    first commit

---
+++ src/main/java/io/mohajistudio/mohagym/MohagymApplication.java
@@ -0,0 +1,13 @@
+package io.mohajistudio.mohagym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohagymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohagymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mohajistudio/mohagym/MohagymApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohajistudio.mohagym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohagymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
