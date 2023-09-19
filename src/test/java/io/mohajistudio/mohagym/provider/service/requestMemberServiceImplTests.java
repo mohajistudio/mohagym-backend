d1a4573477138ea5dcffc524c3dc116b1a44e79f
최민성 <bflook08307@gmail.com>
2023-09-20 00:20:12 +0900

    [Feat] 테스트 코드 완료

--- src/test/java/io/mohajistudio/mohagym/provider/service/requestMemberServiceImplTests.java
+++ src/test/java/io/mohajistudio/mohagym/provider/service/requestMemberServiceImplTests.java
@@ -3,18 +3,18 @@ package io.mohajistudio.mohagym.provider.service;
 import io.mohajistudio.mohagym.core.security.role.Role;
 import io.mohajistudio.mohagym.entity.Member;
 import io.mohajistudio.mohagym.repository.MemberRepository;
-import io.mohajistudio.mohagym.web.dto.responseMember;
 import io.mohajistudio.mohagym.web.dto.requestMember;
+import io.mohajistudio.mohagym.web.dto.requestToken;
 import io.mohajistudio.mohagym.web.dto.requestUserId;
-import org.assertj.core.api.Assertions;
+import io.mohajistudio.mohagym.web.dto.responseMember;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -83,7 +83,7 @@ public class requestMemberServiceImplTests {
         System.out.println("서비스 실행");
         System.out.println(" 서비스 실행 후 user 권한 : " +  usersRole);
         //확인
-        Assertions.assertThat(usersRole).isEqualTo(Role.ADMIN.getDescription());
+        assertThat(usersRole).isEqualTo(Role.ADMIN.getDescription());
     }
 
     @Test
@@ -97,14 +97,44 @@ public class requestMemberServiceImplTests {
                 .build();
         memberServiceImpl.register(request);
         //로그인
-        responseMember token = memberServiceImpl.login(request);
+        responseMember tokens = memberServiceImpl.login(request);
+        //requestToken 로 파싱
+        requestToken tokens2 = requestToken.builder()
+                .accessToken(tokens.getAccessToken())
+                .refreshToken(tokens.getRefreshToken()).build();
         //로그아웃
-        //목 MockHttpServletRequest 객체 설정
-        MockHttpServletRequest mockRequest = new MockHttpServletRequest();
-        mockRequest.addHeader("x-auth-token",token.getAccessToken() );
-        // memberServiceImpl.logout();
+         memberServiceImpl.logout(tokens2);
         Member logoutMember = memberRepository.findByUserId("test");
         System.out.println("로그아웃 후 RefreshToken = " + logoutMember.getRefreshToken());
         assertNull(logoutMember.getRefreshToken());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("토큰 재발급 성공")
+    public void reissueTokenTest() throws InterruptedException {
+        //회원가입
+        requestMember request = requestMember.builder()
+                .userId("test")
+                .password("1234")
+                .build();
+        memberServiceImpl.register(request);
+        //로그인
+        responseMember tokens = memberServiceImpl.login(request);
+        System.out.println("tokens = " + tokens);
+        //requestToken 로 파싱
+        requestToken tokens2 = requestToken.builder()
+                .accessToken(tokens.getAccessToken())
+                .refreshToken(tokens.getRefreshToken()).build();
+        System.out.println("tokens2 = " + tokens2);
+        Thread.sleep(1000); // 로그인과 동시에 재발급이 이루어 지면 만료 일이 같아서 토큰도 같음
+        //토큰 재발급
+        responseMember tokens3 = memberServiceImpl.reissueToken(tokens2);
+        System.out.println("tokens3 = " + tokens3);
+        //엑세스토큰 불일치 확인
+        assertThat(tokens2.getAccessToken()).isNotEqualTo(tokens3.getAccessToken());
+        //리프레시 토큰 불일치 확인
+        assertThat(tokens2.getRefreshToken()).isNotEqualTo(tokens3.getRefreshToken());
+
     }
 }
