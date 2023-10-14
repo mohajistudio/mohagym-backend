8ddeabae7b9a3a178bb17b08765d4d55413a825a
cym1212 <william5680@naver.com>
2023-10-14 14:51:43 +0900

    DTO를 사용하도록 코드를 변경

--- src/main/java/io/mohajistudio/mohagym/attendance_check/controller/AttendanceController.java
+++ src/main/java/io/mohajistudio/mohagym/attendance_check/controller/AttendanceController.java
@@ -1,7 +1,8 @@
 package io.mohajistudio.mohagym.attendance_check.controller;
 
 
-import io.mohajistudio.mohagym.attendance_check.service.AttendanceServiceInterface;
+import io.mohajistudio.mohagym.attendance_check.dto.AttendanceDTO;
+import io.mohajistudio.mohagym.attendance_check.service.AttendanceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +12,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AttendanceController {
 
-    private final AttendanceServiceInterface attendanceServiceInterface;
+    private final AttendanceService attendanceService;
 
-    public AttendanceController(AttendanceServiceInterface attendanceServiceInterface) {
-        this.attendanceServiceInterface = attendanceServiceInterface;
+    public AttendanceController(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
     }
-
 
 
     //id 조회
-    @PostMapping("/members/check/{id}")
-    public ResponseEntity<String> checkId(@PathVariable Long id) {
-        return attendanceServiceInterface.checkId(id);
+    @PostMapping("/members/:memberId/attendance-checks/{id}")
+    public ResponseEntity<AttendanceDTO> checkId(@PathVariable Long id) {
+        return attendanceService.checkId(id);
     }
 
 
+    @GetMapping("/members/:memberId/attendance-checks/{id}")
+    public ResponseEntity<String> attendanceRecord(@PathVariable Long id) {
+        return attendanceService.attendanceRecord(id);
+    }
 
 
 }
--- src/main/java/io/mohajistudio/mohagym/attendance_check/service/AttendanceService.java
+++ src/main/java/io/mohajistudio/mohagym/attendance_check/service/AttendanceService.java
@@ -1,59 +1,57 @@
 package io.mohajistudio.mohagym.attendance_check.service;
 
+import io.mohajistudio.mohagym.attendance_check.dto.AttendanceDTO;
+import io.mohajistudio.mohagym.attendance_check.repository.AttendanceCheckTimeRepository;
 import io.mohajistudio.mohagym.entity.AttendanceCheckTime;
 import io.mohajistudio.mohagym.entity.Member;
-import io.mohajistudio.mohagym.attendance_check.repository.AttendanceCheckTimeRepository;
 import io.mohajistudio.mohagym.repository.MemberRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Slf4j
 @Service
-public class AttendanceService implements AttendanceServiceInterface {
-
+public class AttendanceService {
     private final MemberRepository memberRepository;
     private final AttendanceCheckTimeRepository attendanceCheckTimeRepository;
-
 
     public AttendanceService(MemberRepository memberRepository, AttendanceCheckTimeRepository attendanceCheckTimeRepository) {
         this.memberRepository = memberRepository;
         this.attendanceCheckTimeRepository = attendanceCheckTimeRepository;
     }
 
-    @Override
-    public LocalDateTime getCurrentTime(){
-        return LocalDateTime.now();
-    }
-
-
-    @ResponseBody
-    public ResponseEntity<String> checkId(Long id) {
+    public ResponseEntity<AttendanceDTO> checkId(Long id) {
         Optional<Member> memberEntity = memberRepository.findById(id);
         Map<String, Object> response = new HashMap<>();
         if (memberEntity.isPresent()) {
             Member member = memberEntity.get();
 
             AttendanceCheckTime attendanceCheckTime = new AttendanceCheckTime();
-            attendanceCheckTime.setCreatedAt(LocalDateTime.now());
-
+            attendanceCheckTime.setCreatedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
             attendanceCheckTime.setMember(member);
-            attendanceCheckTimeRepository.save(attendanceCheckTime);
+
+            AttendanceCheckTime savedAttendanceCheckTime = attendanceCheckTimeRepository.save(attendanceCheckTime);
+
+            AttendanceDTO attendanceDTO = new AttendanceDTO();
+            attendanceDTO.setId(savedAttendanceCheckTime.getId());
+            attendanceDTO.setCreatedAt(savedAttendanceCheckTime.getCreatedAt());
 
             response.put("status", "success");
-            response.put("created at", attendanceCheckTime.getCreatedAt());
+            response.put("created at", savedAttendanceCheckTime.getCreatedAt());
 
-            return ResponseEntity.ok(response.toString());
+            return ResponseEntity.ok(attendanceDTO);
         } else {
-            // 회원이 존재하지 않는 경우
             return ResponseEntity.notFound().build();
         }
     }
 
+    @ResponseBody
+    public ResponseEntity<String> attendanceRecord(Long id) {
+        return null;
+    }
 }
