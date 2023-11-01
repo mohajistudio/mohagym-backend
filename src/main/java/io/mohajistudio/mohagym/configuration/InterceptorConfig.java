567b062b379bb4525d3d1b96e3968e3b7b57a61f
cym1212 <william5680@naver.com>
2023-11-01 13:46:21 +0900

    [Refactor] login한 유저만 출척체크를 허용하도록 수정

--- src/main/java/io/mohajistudio/mohagym/configuration/InterceptorConfig.java
+++ src/main/java/io/mohajistudio/mohagym/configuration/InterceptorConfig.java
@@ -19,7 +19,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-        List<String> excludePatterns = Arrays.asList("/h2-console/**","/favicon.ico","/register","/login","/token","/members/**");
+        List<String> excludePatterns = Arrays.asList("/h2-console/**","/favicon.ico","/register","/login","/token");
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/**")// 전체 경로에 대해서 인터셉터 등록
                 .excludePathPatterns(excludePatterns);//해당 경로 인터셉터 예외 설정
